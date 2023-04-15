7096321a7dfe801a3edf926fa6be282d7128879a
Josh Guha <joshguha@gmail.com>
2023-04-15 09:33:58 +0100

    Add mock images

--- app/src/components/AuctionProfile.tsx
+++ app/src/components/AuctionProfile.tsx
@@ -8,6 +8,15 @@ import { formatFixed } from "@ethersproject/bignumber";
 import { formatTimestamp } from "@/utils/formatTimestamp";
 import { convertSecondsToDays } from "@/utils/convertSecondsToDays";
 
+import img1 from "../../public/fakeApes/1.jpg";
+import img2 from "../../public/fakeApes/2.jpg";
+import img3 from "../../public/fakeApes/3.jpg";
+import img4 from "../../public/fakeApes/4.jpg";
+import img5 from "../../public/fakeApes/5.jpg";
+import img6 from "../../public/fakeApes/6.jpg";
+
+const images = [img1, img2, img3, img4, img5, img6];
+
 const AuctionProfile = ({ auction }: { auction: Auction }) => {
   const { setLocation, setSelectedAuction } = useContext(NavigationContext);
 
@@ -31,7 +40,13 @@ const AuctionProfile = ({ auction }: { auction: Auction }) => {
       className="p-5 m-5 flex flex-col space-y-3 w-64 bg-offBlack-10 rounded-lg drop-shadow-xl hover:-translate-y-2 transition animate-fadeIn cursor-pointer"
       onClick={onClick}
     >
-      <img src={""} alt="nft" width="250" height="250" />
+      <img
+        src={images[auction.nftCollateralTokenId.toNumber() % 6].src}
+        alt="nft"
+        width="250"
+        height="250"
+        style={{ borderRadius: "10px" }}
+      />
       <div className="flex flex-col space-y-1">
         <p className={`text-2xl ${russo.variable} font-heading`}>
           {auction.collectionName}
