71a9f14c2631577eaf16bb74eeaac7fea3d557af
Josh Guha <joshguha@gmail.com>
2023-04-15 04:36:53 +0100

    Update rainbowkit theme

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,7 +1,11 @@
 import "@/styles/globals.css";
 import "../styles/globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
-import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  lightTheme,
+} from "@rainbow-me/rainbowkit";
 import type { AppProps } from "next/app";
 import { configureChains, createClient, WagmiConfig } from "wagmi";
 import { mainnet, polygon } from "wagmi/chains";
@@ -28,7 +32,13 @@ const wagmiClient = createClient({
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains}>
+      <RainbowKitProvider
+        chains={chains}
+        theme={lightTheme({
+          accentColor: "#17ff49",
+          accentColorForeground: "#010501",
+        })}
+      >
         <AllContextsProvider>
           <Component {...pageProps} />
         </AllContextsProvider>
